dbdd189983fed1c34c05fee2622db99bb16b4b8e
Ben Huynh <102061216+bdhuynh14@users.noreply.github.com>
2022-04-19 15:29:11 -0400

    inclusion of data models 4/19/2022

---
+++ src/main/java/learn/house/data/ReservationRepository.java
@@ -0,0 +1,16 @@
+package learn.house.data;
+
+import learn.house.models.Reservation;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ReservationRepository {
+    List<Reservation> findById(String hostId) throws DataException;
+
+    Reservation add(String hostId, Reservation reservation) throws DataException;
+
+    boolean update(String hostId, Reservation reservation) throws DataException;
+
+    Boolean delete(String hostId, Reservation reservation) throws DataException;
+}
